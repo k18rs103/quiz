59a9b69c325d59326c9347973599874e8089c10d
k18rs103 <k18rs103@st.kyusan-u.ac.jp>
2019-12-16 23:35:47 +0900

    データストアにデータを保存

---
+++ www/components/js/app.js
@@ -0,0 +1,27 @@
+// This is a JavaScript file
+//mobile backendのAPIキーを設定
+var ncmb = new NCMB("YOUR_APPLICATION_KEY","YOUR_CLIENTKEY");
+
+
+//データをmobile backendに保存するメソッド
+function saveData(){
+    //クラス名を指定して新規クラスを作成
+    var Data = ncmb.DataStore("Data");
+
+    //Dataクラスのインスタンスを作成
+    var data = new Data();
+
+    //作成したインスタンスのaisatsuというフィールドに文字データを設定
+    data.set("aisatsu", "hello, world!");
+
+    //設定したデータをmobile backendに保存
+    data.save()
+        .then(function(object) {
+              //成功する時の処理
+              $("#message").html("<p>データ保存に成功!</p>");
+          })
+        .catch(function(error) {
+              //エラーが発生する時の処理
+              $("#message").html("error:" + error.message);
+          });
+}
